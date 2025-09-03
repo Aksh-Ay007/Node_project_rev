be4b77a721238aa57a77323ef8173ec391aaec4e
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-09-03 20:55:19 +0400

    route handlerrs

--- src/app.js
+++ src/app.js
@@ -2,47 +2,38 @@ const express = require("express");
 
 const app = express();
 
-
-
-
 //route handler
 
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("iam first");
+      next();
+    },
 
-app.use('/user',[(req,res,next)=>{
+    (req, res, next) => {
+      console.log("second");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("3rd");
 
-  console.log('iam first');
-  next()
-},
+    next();
+  },
 
-(req,res,next)=>{
+  (req, res, next) => {
+    console.log("4th rsponse");
 
-  console.log('second');
-  next()
-}],
-(req,res,next)=>{
+    next();
+  },
 
-  console.log('3rd');
-
-  next()
-},
-
-(req,res,next)=>{
-
-  console.log('4th rsponse');
-
-  next()
-},
-
-(req,res,next)=>{
-
-  console.log('5 th response');
-  res.send('iam 5 the one😌')
-
-}
-
-
-
-)
+  (req, res, next) => {
+    console.log("5 th response");
+    res.send("iam 5 the one😌");
+  }
+);
 
 app.listen(3000, () => {
   console.log("server is running");
