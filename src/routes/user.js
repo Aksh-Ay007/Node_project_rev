388a72970cd211ec816b1ca4aa20a9f904a26b42
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-09-09 00:28:17 +0400

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User=require('../models/user')
 
 const userRouter = express.Router();
+
+const userData=['firstName','lastName','about','skills','photoUrl']
 
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
   try {
@@ -66,6 +69,34 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({ message: "your connection requests are: ", connections: data });
   } catch (error) {
     res.status(400).send("something went wrong " + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loginUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ senderUserId: loginUser._id }, { receiverUserId: loginUser._id }],
+    }).select("senderUserId receiverUserId");
+
+    const hideUsers = new Set();
+
+    connectionRequest.forEach((req) => {
+      hideUsers.add(req.senderUserId.toString());
+      hideUsers.add(req.receiverUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsers) } },
+        { _id: { $ne: loginUser._id } },
+      ],
+    }).select(userData);
+
+    res.json({message:"please find the feed users",users})
+  } catch (error) {
+    res.status(400).send("something went wrong..!", error.message);
   }
 });
 
